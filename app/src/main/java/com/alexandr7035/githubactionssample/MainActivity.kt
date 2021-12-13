e41c5be8d67a662d889a6cb6adfee544935cf762
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-13 21:40:31 +0300

    Initial commit

---
+++ app/src/main/java/com/alexandr7035/githubactionssample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.alexandr7035.githubactionssample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
