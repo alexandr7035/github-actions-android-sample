5d66097b652050e5d500a8a9e479e4c373b2320c
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-12-13 22:05:48 +0300

    Show version in activity

--- app/src/main/java/com/alexandr7035/githubactionssample/MainActivity.kt
+++ app/src/main/java/com/alexandr7035/githubactionssample/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.alexandr7035.githubactionssample
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val versionView = findViewById<TextView>(R.id.versionView)
+        versionView.text = "Version ${BuildConfig.VERSION_NAME}"
     }
 }
